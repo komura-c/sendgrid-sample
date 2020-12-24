9a74a2010348a7f9f0705724285ffe95b27364be
komura <affirman03139@outlook.jp>
2020-12-25 01:25:38 +0900

    feat(init): angularfire設定

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBWpdqH5njlPsZVCFeJElqt2juiyCo2lSg',
+    authDomain: 'sendgrid-sample-31052.firebaseapp.com',
+    projectId: 'sendgrid-sample-31052',
+    storageBucket: 'sendgrid-sample-31052.appspot.com',
+    messagingSenderId: '45184438413',
+    appId: '1:45184438413:web:89626caf3e8d5658d252c4',
+    measurementId: 'G-FZLX5NFC93',
+  },
 };
 
 /*
